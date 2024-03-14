50bcbb835b33318a2a7edf59e1b36d6c3cc5c1ae
KNGShiranui <mathematics9973@gmail.com>
2024-03-15 00:00:12 +0900

    ログイン機能

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// viteつかうならこのファイルは不要な気がする
 import "@hotwired/turbo-rails"
-import "controllers"
+import "controllers/application"
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :tasks, only: %i(index create update destroy)
-    end
-  end
   
   devise_for :users, controllers: {
     sessions: 'users/sessions',
@@ -13,10 +8,15 @@ Rails.application.routes.draw do
   devise_scope :user do
     delete '/user_logout', to: 'devise/sessions#destroy', as: :logout_user
   end
-
+  
   resources :tops, only: %i(index)
   resources :users, only: %i(index show)
   resources :tasks
+  namespace :api do
+    namespace :v1 do
+      resources :tasks, only: %i(index create update destroy)
+    end
+  end
   resources :blogs
 
   root 'tops#index'
